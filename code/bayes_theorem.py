7cb9cbede79ed1b091188781a48f3994ee7d934b
Adam Mantz <amantz@slac.stanford.edu>
2017-01-13 16:07:50 -0800

    minor tweaks to bayes chunk & graphics

--- code/bayes_theorem.py
+++ code/bayes_theorem.py
@@ -10,20 +10,17 @@ import scipy.stats as st
 def GammaPDF(x,a,b):
     return b**a * x**(a-1.0) * np.exp(-b*x) / Gamma(a)
 
-def bayesDemo(alpha0, beta0, N, showLikelihood=False):
+def bayesDemo(alpha0, beta0, N):
     mu = np.linspace(0.0, np.max([10.0, N+5.0*np.sqrt(N)]), 100)
     prior = GammaPDF(mu, alpha0, beta0)
     like = st.poisson.pmf(N,mu)
     postpar = (alpha0+N, beta0+1)
     post = GammaPDF(mu, postpar[0], postpar[1])
-    postLine, = plt.plot(mu,post)
-    priorLine, = plt.plot(mu,prior)
-    if showLikelihood:
-        likeLine, = plt.plot(mu,like)
-        plt.legend((priorLine, likeLine, postLine), ('Prior', 'Likelihood', 'Posterior'))
-    else:
-        dataLine, = plt.plot([N]*2, [0.0, np.max(post)*1.1], '--')
-        plt.legend((priorLine, dataLine, postLine), ('Prior', 'Measurement', 'Posterior'), fontsize='x-large')
+    priorLine, = plt.plot(mu,prior,'g')
+    likeLine, = plt.plot(mu,like,'r')
+    dataLine, = plt.plot([N]*2, [0.0, np.max(post)*1.1], 'k--')
+    postLine, = plt.plot(mu,post,'b')
+    plt.legend((priorLine, dataLine, likeLine, postLine), ('Prior', 'Data', 'Likelihood', 'Posterior'))
     plt.xlabel(r'$\mu$', fontsize=22)
     plt.ylabel('density', fontsize=22)
     return postpar
