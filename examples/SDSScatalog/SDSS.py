ee30e5473368ba556b5fd7788e27bf3f5335a4fb
Phil Marshall <dr.phil.marshall@gmail.com>
2017-01-15 20:15:51 -0800

    Fixed print call for python 3

--- examples/SDSScatalog/SDSS.py
+++ examples/SDSScatalog/SDSS.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from __future__ import print_function
 
 def select(sql, filename='SDSSobjects.csv'):
     """
@@ -20,7 +21,7 @@ def select(sql, filename='SDSSobjects.csv'):
     # sdssdata is a Table object that can be saved in a csv file with:
     from astropy.io import ascii
     ascii.write(sdssdata, filename, format='csv', fast_writer=False)
-    print "SDSS data downloaded and written to "+filename
+    print('SDSS data downloaded and written to '+filename)
     # Return a dataframe to the command line as well.
     return pd.read_csv(filename)
 
