09ba25d16bc628a4bd06b9fe1a3898a9e3bb3cad
Adam Mantz <amantz@slac.stanford.edu>
2022-12-29 11:51:35 -0800

    tutorials

--- tutorials/tbc.py
+++ tutorials/tbc.py
@@ -1,8 +1,29 @@
 class SolutionMissingError(Exception):
     def __init__(self):
-        Exception.__init__(self, "You tried to call code that has not been completed!")
+        Exception.__init__(self, "This code/cell still needs to be completed!")
 
-def TBC(ignoredArg=None):
-    raise SolutionMissingError
+class ToBeCompleted:
+    def __init__(self, tutorial_name):
+        self._path = 'completions/' + tutorial_name + '/'
+        try:
+            f = open(self._path + 'files.txt')
+            self._files = [s.strip('\n').strip() for s in f.readlines()]
+            f.close()
+            self._raise = False
+            self._i = -1
+        except:
+            self._raise = True
+    def __call__(self, index=None):
+        if self._raise:
+            raise SolutionMissingError
+        else:
+            if index is None:
+                self._i += 1
+            else:
+                self._i = index
+            #print('Executing completion', self._files[self._i])
+            exec(open(self._path + self._files[self._i]).read(), globals())
 
-TBC_above = TBC
+TBC = ToBeCompleted(TutorialName) # TutorialName must be defined at global scope already
+
+TBC_above = TBC # alias
